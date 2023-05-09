a0bc2eae4c73c8037535951c325ca2d365acacc1
lanlonglong <lieorea@gmail.com>
2023-05-09 15:33:17 +0800

    init

---
+++ A883056-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A883056_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
