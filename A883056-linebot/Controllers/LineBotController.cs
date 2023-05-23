071c95481e874018f31a377da0e624b68ce13522
lanlonglong <lieorea@gmail.com>
2023-05-23 16:48:31 +0800

    還未測試

--- A883056-linebot/Controllers/LineBotController.cs
+++ A883056-linebot/Controllers/LineBotController.cs
@@ -22,7 +22,9 @@ namespace A883056_linebot.Controllers
         {
             _httpContextAccessor = serviceProvider.GetRequiredService<IHttpContextAccessor>();
             _httpContext = _httpContextAccessor.HttpContext;
-            _lineBotConfig = lineBotConfig;
+            _lineBotConfig = new LineBotConfig();
+            _lineBotConfig.channelSecret = "175a36eb0d9cc08b5f1649a5ff7b1076";
+            _lineBotConfig.accessToken = "HPOfFSpAHqf+AFlNQUN5+0Qu7yDuJX3eyo5g6ghJI8Pk1WMjtAS6JFqpTBoLHm0sWFYvvlzxjXR0Pti2AGEWa/XoxcFj2j8HEVDV7OMx43a+oW5XrIK3w5fePaAFOEBPBXm4fMXGACXwizDU8esYywdB04t89/1O/w1cDnyilFU=";
         }
 
         
